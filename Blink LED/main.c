e3d69cc950eaa6642ee76ef2128276c2530a1d4f
levietduc0712 <levietduc0712@gmail.com>
2023-11-22 18:44:56 +0700

    first commit

--- Blink LED/main.c
+++ Blink LED/main.c
@@ -20,8 +20,8 @@ int main(void)
     WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
 
     // Configure GPIO
-    P1DIR |= BIT0;                          // P1DIR |= 0x0001 <=> P1PIR |= 00000001. Set Pin 1 of Port 1 as an output.
-    P1OUT |= BIT0;                          // Set output of Pin 1 - Port 1 is high
+    P1DIR |= BIT0;                          // P1DIR |= 0x0001 <=> P1PIR |= 00000001. Set Pin 0 of Port 1 as an output.
+    P1OUT |= BIT0;                          // Set output of Pin 0 - Port 1 is high
 
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
                                             // to activate previously configured port settings

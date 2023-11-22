3f1aa8bf3180b5a247e40ea3f3ae2649e35894dc
levietduc0712 <levietduc0712@gmail.com>
2023-11-22 18:04:19 +0700

    first commit

---
+++ Blink LED/main.c
@@ -0,0 +1,37 @@
+//******************************************************************************
+//  MSP430FR69x Demo - Toggle P1.0 using software
+//
+//  Description: Toggle P1.0 using software.
+//  ACLK = n/a, MCLK = SMCLK = default DCO
+//
+//           MSP430FR6989
+//         ---------------
+//     /|\|               |
+//      | |               |
+//      --|RST            |
+//        |               |
+//        |           P1.0|-->LED
+//
+//   Viet Duc
+//   Nov 2023
+//   Built with Code Composer Studio V10.2
+//******************************************************************************
+#include <msp430.h>
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
+
+    // Configure GPIO
+    P1DIR |= BIT0;                          // Clear P1.0 output latch for a defined power-on state
+    P1OUT |= BIT0;                          // Set P1.0 to output direction
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
+
+    while(1)
+    {
+        P1OUT ^= BIT0;                      // Toggle LED
+        __delay_cycles(100000);
+    }
+}

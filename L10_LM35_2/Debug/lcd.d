602d2d3e9b9b3598485de804af268009e57c6c03
levietduc0712 <levietduc0712@gmail.com>
2023-12-31 13:12:16 +0700

    first commit

---
+++ L10_LM35_2/Debug/lcd.d
@@ -0,0 +1,24 @@
+# FIXED
+
+lcd.obj: ../lcd.c
+lcd.obj: ../lcd.h
+lcd.obj: D:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h
+lcd.obj: D:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430fr6989.h
+lcd.obj: D:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h
+lcd.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+lcd.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+
+../lcd.c:
+
+../lcd.h:
+
+D:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h:
+
+D:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430fr6989.h:
+
+D:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+

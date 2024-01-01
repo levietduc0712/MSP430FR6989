8b3f9b51b871ed8318187fc40cbffc7e7684b3a3
levietduc0712 <levietduc0712@gmail.com>
2024-01-01 12:49:16 +0700

    first commit

---
+++ A1_LM35_UART_Matlab/A1_LM35_UART_LCD/Debug/main.d
@@ -0,0 +1,45 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h
+main.obj: D:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430fr6989.h
+main.obj: D:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h
+main.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdio.h
+main.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+main.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+main.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdarg.h
+main.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+main.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+main.obj: D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+main.obj: ../lcd.h
+
+../main.c:
+
+D:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h:
+
+D:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430fr6989.h:
+
+D:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdio.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdarg.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+D:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+../lcd.h:
+
